6be3c14a670ee4910d56150354b9e6636385ed64
lyp1noff <lyp1noff@gmail.com>
2022-06-24 16:13:07 +0300

    fix

--- bot.py
+++ bot.py
@@ -1,11 +1,25 @@
+import json
 import telebot
+from downloader import *
 from dotenv import load_dotenv
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
-from downloader import *
 
 load_dotenv()
 token = os.getenv("TG_TOKEN")
 bot = telebot.TeleBot(token, parse_mode=None)
+
+users = {}
+
+
+def update_users_write():
+    with open('./users.json', 'w', encoding='UTF-8') as write_users:
+        json.dump(users, write_users, ensure_ascii=False, indent=4)
+
+
+def update_users_read():
+    global users
+    with open("./users.json", 'r', encoding='UTF-8') as read_users:
+        users = json.load(read_users)
 
 
 def get_songs_list_markup(message, song_req, page_num):
@@ -28,16 +42,45 @@ def get_songs_list_markup(message, song_req, page_num):
     return markup
 
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
+@bot.message_handler(commands=['start'])
+def start(message):
+    if str(message.from_user.id) in users.keys():
+        pass
+    elif str(message.from_user.id) not in users.keys():
+        users[str(message.from_user.id)] = {
+            "without_caption": 0
+        }
+        update_users_write()
     bot.reply_to(message, "Send me song name, son of bitch <3")
+
+
+@bot.message_handler(commands=['help'])
+def stop(message):
+    bot.reply_to(message, "help urself))0)")
+
+
+@bot.message_handler(commands=['stop'])
+def stop(message):
+    bot.reply_to(message, "ama unstoppable")
+
+
+@bot.message_handler(commands=['switch_caption'])
+def switch_caption(message):
+    if users[str(message.from_user.id)]["without_caption"]:
+        users[str(message.from_user.id)]["without_caption"] = 0
+        bot.send_message(message.chat.id, "Caption enabled")
+    else:
+        users[str(message.from_user.id)]["without_caption"] = 1
+        bot.send_message(message.chat.id, "Caption disabled")
+    update_users_write()
 
 
 @bot.message_handler(func=lambda m: True)
 def msg_handler(message):
     song_name = message.text
     markup = get_songs_list_markup(message, song_name, 1)
-    bot.send_message(message.chat.id, "Choose one of 'em", reply_markup=markup)
+    if markup:
+        bot.send_message(message.chat.id, "Choose one of 'em", reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -71,11 +114,16 @@ def callback_query(call):
         song_name_full = filename[:-4]
         song_name = song_name_full[song_name_full.find(' - ') + 3:]
         song_artist = song_name_full[:song_name_full.find(' - ')]
-        bot.send_audio(message.chat.id, audio, title=song_name, performer=song_artist)
+        caption = "[via](https://t.me/lyp1noff_music_bot)"
+        if users[str(call.from_user.id)]["without_caption"]:
+            caption = ""
+        bot.send_audio(message.chat.id, audio, caption=caption,
+                       title=song_name, performer=song_artist, parse_mode="MarkdownV2")
     else:
         print("wrong callback data")
 
 
 if __name__ == '__main__':
+    update_users_read()
     print("Started")
     bot.infinity_polling()
